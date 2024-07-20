5e04acf5da885db6f6c45e4706010b4e5b39a600
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-20 09:23:34 +0200

    feat: load views lazy in scroll view

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,14 +7,29 @@
 
 import SwiftUI
 
+struct CustomTextView: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(text: String) {
+        print("CUstom new custom text: \(text)")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Image(.tuscany)
-            .resizable()
-            .scaledToFit()
-            .containerRelativeFrame(.horizontal) { size, axis in
-                size * 0.8
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomTextView(text: "Row \($0)")
+                }
             }
+            .frame(maxWidth: .infinity)
+        }
     }
 }
 
